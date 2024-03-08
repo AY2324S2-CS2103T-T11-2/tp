d76e87013187287fc8046c4078e9cc6f3e8d262f
Hibeom0929 <E1093045@u.nus.edu>
2024-03-08 17:05:05 +0800

    Add a new remark command

---
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -0,0 +1,38 @@
+package seedu.address.model.person;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
+public class Remark {
+    public final String value;
+
+    public Remark(String remark) {
+        requireNonNull(remark);
+        value = remark;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof Phone)) {
+            return false;
+        }
+
+        String otherValue = ((Remark) other).value;
+        return value.equals(otherValue);
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+}
